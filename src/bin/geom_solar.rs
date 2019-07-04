46ae7ab1e5f56f50772c10bd252c6aa30012c9e6
axect <axect@outlook.kr>
2019-07-04 21:57:19 +0900

    New unit - Geom_Solar

---
+++ src/bin/geom_solar.rs
@@ -0,0 +1,14 @@
+extern crate natural_unit;
+use natural_unit::*;
+
+fn main() {
+    let m_solar = CONSTANT_CGS.m_solar;
+    let c = CONSTANT_CGS.c;
+    let G = CONSTANT_CGS.G;
+    let cgs_to_geom = ConversionFactor::new(
+        1f64 / m_solar,
+        c.powi(2) / (G * m_solar),
+        c.powi(3) / (G * m_solar),
+    );
+    println!("{:#?}", cgs_to_geom);
+}
